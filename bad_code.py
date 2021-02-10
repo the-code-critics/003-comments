c46c29e79d00b63a4398b5cfb80314531cd6e8b4
Stefan Maetschke <stefan.maetschke@gmail.com>
2021-02-10 20:08:13 +1100

    code examples added

--- bad_code.py
+++ bad_code.py
@@ -1,3 +1,34 @@
-
 # increment i by one
 i += 1
+
+
+def sentence_similarity(sentence1, sentence2):
+    """ compute the sentence similarity using Wordnet """
+    # Tokenize and tag
+    sentence1 = pos_tag(word_tokenize(sentence1))
+    sentence2 = pos_tag(word_tokenize(sentence2))
+
+    # Get the synsets for the tagged words
+    synsets1 = [tagged_to_synset(*tagged_word) for tagged_word in sentence1]
+    synsets2 = [tagged_to_synset(*tagged_word) for tagged_word in sentence2]
+
+    # Filter out the Nones
+    synsets1 = [ss for ss in synsets1 if ss]
+    synsets2 = [ss for ss in synsets2 if ss]
+
+    score, count = 0.0, 0
+    # For each word in the first sentence
+    for synset in synsets1:
+        # Get the similarity value of the most similar word in the other
+        # sentence
+        best_score = max([synset.path_similarity(ss) for ss in synsets2])
+
+        # Check that the similarity could have been computed
+        if best_score is not None:
+            score += best_score
+            count += 1
+
+    # Average the values
+    score /= count
+    return score
+
--- good_code.py
+++ good_code.py
@@ -1,2 +1,26 @@
-def increment(i, by=1):
-    return i + by
+def increment(x, by=1):
+    return x + by
+
+
+def tokenize(sentence):
+    return pos_tag(word_tokenize(sentence))
+
+
+def synsets(sentence):
+    tokens = tokenize(sentence)
+    return remove_none(tagged_to_synset(*t) for t in tokens)
+
+
+def remove_none(xs):
+    return [x for x in xs if x is not None]
+
+
+def score(synset, synsets):
+    return max(synset.path_similarity(s) for s in synsets)
+
+
+def sentence_similarity(sentence1, sentence2):
+    synsets1 = synsets(sentence1)
+    synsets2 = synsets(sentence2)
+    scores = remove_none(score(s, synsets2) for s in synsets1)
+    return sum(scores) / len(scores)
